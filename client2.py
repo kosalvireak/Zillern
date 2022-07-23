7e3241e922ed3aac83f5eb48754b767cdf74b815
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-23 14:30:17 +0700

    add server & client 2=

---
+++ client2.py
@@ -0,0 +1,16 @@
+
+import socket
+from datetime import datetime
+HOST= "172.16.2.87"
+PORT = 6789
+max_size=1024
+print("Starting the client at: ", datetime.now())
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)                    #start socket
+s.connect((HOST,PORT))                                                  #connect to server
+while True:
+    message_to_server = input("Enter message to server: ")              #input message
+    message_to_server_encode = message_to_server.encode('utf-8')        #encode input message
+    s.send(message_to_server_encode)                                    #send to server
+    data = s.recv(max_size)                                             #receive from server
+    print("At ", datetime.now(), "server replied with: ", data.decode('utf-8')) #print it out
+s.close()
---
+++ server2.py
@@ -0,0 +1,24 @@
+import socket
+from datetime import datetime
+HOST= "172.20.10.4"
+PORT = 6789
+max_size=1024
+sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+sock.bind((HOST,PORT)) #open server
+print('Starting The Server at: ', datetime.now())
+print("Waiting For The Incoming Connection from client")
+sock.listen(5) #listening client
+client, addr = sock.accept()  #client connect
+while True:
+    data = client.recv(max_size)   #recieve from client
+    if data.decode('utf-8') == 'q': #decode message
+        break
+    print("At ", datetime.now(),addr, " said: ", data.decode('utf-8'))
+    message_to_client = input("Enter message to client: ")   #input message
+    message_to_client_encode = message_to_client.encode('utf-8') #encode message
+    client.send(message_to_client_encode)    #send to client back
+    if message_to_client == 'q':
+        break
+
+client.close()
+sock.close()
