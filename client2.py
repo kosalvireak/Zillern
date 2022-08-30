eb0d7a4d0d1a39cb1a470248ba89a168847341b2
Kosalvireak Khoeun <virakvary@gmail.com>
2022-08-30 07:36:52 +0700

    add if server send text

--- client2.py
+++ client2.py
@@ -1,16 +1,26 @@
 
 import socket
 from datetime import datetime
-HOST= "172.16.2.87"
+HOST= "192.168.56.1"
 PORT = 6789
 max_size=1024
 print("Starting the client at: ", datetime.now())
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)                    #start socket
-s.connect((HOST,PORT))                                                  #connect to server
+s.connect((HOST,PORT))
+print("""q : for close connection
+         on : for turn a light on
+         off: for turn a light off
+        """)                                                  #connect to server
 while True:
     message_to_server = input("Enter message to server: ")              #input message
     message_to_server_encode = message_to_server.encode('utf-8')        #encode input message
     s.send(message_to_server_encode)                                    #send to server
     data = s.recv(max_size)                                             #receive from server
+    if data.decode('utf-8') == 'on': 
+        #led.on()
+        print("light is on")
+    elif data.decode('utf-8') == 'off':
+        #led.off()
+        print("light is off")
     print("At ", datetime.now(), "server replied with: ", data.decode('utf-8')) #print it out
 s.close()
--- server2.py
+++ server2.py
@@ -1,6 +1,6 @@
 import socket
 from datetime import datetime
-HOST= "172.20.10.4"
+HOST= "192.168.56.1"
 PORT = 6789
 max_size=1024
 sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
@@ -9,8 +9,12 @@ print('Starting The Server at: ', datetime.now())
 print("Waiting For The Incoming Connection from client")
 sock.listen(5) #listening client
 client, addr = sock.accept()  #client connect
+print("""q : for close connection
+         on : for turn the light on
+         off: for turn the light off
+        """)
 while True:
-    data = client.recv(max_size)   #recieve from client
+    data = client.recv(max_size)   #receive from client
     if data.decode('utf-8') == 'q': #decode message
         break
     print("At ", datetime.now(),addr, " said: ", data.decode('utf-8'))
