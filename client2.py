d27f00bc106cf8f4a4b4c07114ed242ea9ca190b
Kosalvireak Khoeun <virakvary@gmail.com>
2022-08-30 08:25:16 +0700

    server can talk with client

--- client2.py
+++ client2.py
@@ -1,26 +1,41 @@
 
 import socket
 from datetime import datetime
-HOST= "192.168.56.1"
+HOST = "172.20.10.4"
 PORT = 6789
-max_size=1024
+max_size = 1024
+
+FORMAT = "utf-8"
 print("Starting the client at: ", datetime.now())
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)                    #start socket
-s.connect((HOST,PORT))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # start socket
+client.connect((HOST, PORT))
 print("""q : for close connection
 on : for turn a light on
 off: for turn a light off
-        """)                                                  #connect to server
+        """)  # connect to server
 while True:
-    message_to_server = input("Enter message to server: ")              #input message
-    message_to_server_encode = message_to_server.encode('utf-8')        #encode input message
-    s.send(message_to_server_encode)                                    #send to server
-    data = s.recv(max_size)                                             #receive from server
-    if data.decode('utf-8') == 'on': 
-        #led.on()
+    # message_to_server = input("Enter message to server: ")              #input message
+    # message_to_server_encode = message_to_server.encode('utf-8')        #encode input message
+    # s.send(message_to_server_encode)                                    #send to server
+    data = client.recv(max_size)  # receive from server
+    if data.decode('utf-8') == 'on':
+        # led_server.on()
         print("light is on")
     elif data.decode('utf-8') == 'off':
-        #led.off()
+        # led_server.off()
         print("light is off")
-    print("At ", datetime.now(), "server replied with: ", data.decode('utf-8')) #print it out
-s.close()
+    print("At ", datetime.now(), "server replied with: ",
+          data.decode('utf-8'))  # print it out
+    if data.decode('utf-8') == 'q':
+        break
+
+f = open("/home/pi/Desktop/Light sensor.txt", "r")
+data = f.read()
+client.send("Light sensor.txt".encode(FORMAT))
+msg = client.recv(max_size).decode(FORMAT)
+print(f"server: {msg}")
+client.send(data.encode(FORMAT))
+msg = client.recv(max_size).decode(FORMAT)
+print(f"server: {msg}")
+f.close()
+client.close()
--- server2.py
+++ server2.py
@@ -1,6 +1,6 @@
 import socket
 from datetime import datetime
-HOST= "192.168.56.1"
+HOST = socket.gethostbyname(socket.gethostname())
 PORT = 6789
 max_size=1024
 sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
@@ -14,10 +14,10 @@ on : for turn the light on
 off: for turn the light off
         """)
 while True:
-    data = client.recv(max_size)   #receive from client
-    if data.decode('utf-8') == 'q': #decode message
-        break
-    print("At ", datetime.now(),addr, " said: ", data.decode('utf-8'))
+    # data = client.recv(max_size)   #receive from client
+    # if data.decode('utf-8') == 'q': #decode message
+    #     break
+    #print("At ", datetime.now(),addr, " said: ", data.decode('utf-8'))
     message_to_client = input("Enter message to client: ")   #input message
     message_to_client_encode = message_to_client.encode('utf-8') #encode message
     client.send(message_to_client_encode)    #send to client back
